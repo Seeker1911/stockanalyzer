1e7973e6f1533efeebc88142952f5cb5fb425ec6
michael.mead <michael.mead@vanderbilt.edu>
2018-07-29 19:29:36 -0500

    refactor postgres script

--- create_db.sh
+++ create_db.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 psql -U postgres -h localhost -f sql_files/create_db.sql
-#source sql_files/create_db.sql
-##-- Connect to the database
-#\c tutorial
-#
-psql -U postgres -h localhost -d tutorial
+psql -U postgres -h localhost -d testdb
--- sql_files/create_db.sql
+++ sql_files/create_db.sql
@@ -1,21 +1,25 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
-drop database if exists tutorial;
-CREATE database tutorial owner postgres;
-\connect tutorial
+drop database if exists testdb;
+drop table if exists quotes;
+CREATE database testdb owner postgres;
+\connect testdb
 -- Extend the database with TimescaleDB
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 -- We start by creating a regular SQL table
 
-CREATE TABLE conditions (
-  time        TIMESTAMPTZ       NOT NULL,
-  location    TEXT              NOT NULL,
-  temperature DOUBLE PRECISION  NULL,
-  humidity    DOUBLE PRECISION  NULL
+CREATE TABLE quotes (
+  time         TIMESTAMPTZ       NOT NULL,
+  symbol       varchar(4)        NOT NULL,
+  company_name TEXT              NOT NULL,
+  close        DOUBLE PRECISION  NULL,
+  open         DOUBLE PRECISION  NULL,
+  high         DOUBLE PRECISION  NULL,
+  low          DOUBLE PRECISION  NULL
 );
 -- This creates a hypertable that is partitioned by time
 --   using the values in the `time` column.
 
-SELECT create_hypertable('conditions', 'time');
-INSERT INTO conditions(time, location, temperature, humidity)
-  VALUES (NOW(), 'office', 70.0, 50.0);
+SELECT create_hypertable('quotes', 'time');
+INSERT INTO quotes(symbol, time, company_name, close, open, high, low)
+  VALUES ('AAPL', NOW(), 'Apple computer', 700.0, 500.0, 1000.0, 200.0);
